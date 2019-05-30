60ab04325cbe7fa060b31b9a1bdddfcd1ba4ad0d
Paul Black <pblack67@comcast.net>
2019-05-30 11:30:28 -0500

    Roughed out architecture, added viewProducts support

--- bamazonManager.js
+++ bamazonManager.js
@@ -22,14 +22,50 @@ function mainMenu() {
             name: 'option',
             message: 'What do you want to do?',
             choices: [
-              'View Products for Sale',
-              'View Low Inventory',
-              'Add to Inventory',
-              'Add New Product'
+                'View Products for Sale',
+                'View Low Inventory',
+                'Add to Inventory',
+                'Add New Product'
             ]
-          }
+        }
     ).then(answers => {
         console.log(answers);
-        mainMenu();
+        switch (answers.option) {
+            case 'View Products for Sale':
+                viewProducts();
+                break;
+
+            case 'View Low Inventory':
+                viewLowInventory();
+                break;
+
+            case 'Add to Inventory':
+                addIventory();
+                break;
+
+            case 'Add New Product':
+                addProduct();
+                break;
+        }
     });
+}
+
+function viewProducts() {
+    connection.query("SELECT * FROM products", (err, result) => {
+        if (err) throw err;
+        console.table(result);
+        mainMenu();
+    })
+}
+
+function viewLowInventory() {
+
+}
+
+function addIventory() {
+
+}
+
+function addProduct() {
+
 }
