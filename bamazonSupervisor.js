6939a708247db492680e8255450079ce2b929906
Paul Black <pblack67@comcast.net>
2019-05-31 08:55:06 -0500

    Added fancy join query to compute total sales by department and profit for each department

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -45,7 +45,23 @@ function mainMenu() {
 }
 
 function viewSalesByDepartment() {
-    mainMenu();
+    connection.query(`
+SELECT a.department_id AS 'Department ID', 
+a.department_name AS 'Department Name', 
+a.over_head_costs AS 'Overhead Costs', 
+SUM(b.product_sales) AS 'Total Sales',
+(SUM(b.product_sales) - a.over_head_costs) AS 'Total Profit'
+FROM departments AS a
+INNER JOIN products AS b ON a.department_name = b.department_name
+GROUP BY a.department_name
+ORDER BY a.department_name;
+`,
+        (err, result) => {
+            if (err) throw err;
+            console.table(result);
+            mainMenu();
+        }
+    )
 }
 
 function viewDepartments() {
